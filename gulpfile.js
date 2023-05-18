a22f8978e16f2ec40942940345077eb2c98b7b9e
aadhunique-developer <developer@aadhunique.com>
2023-05-18 15:54:38 -0400

    Updated Docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,28 @@
 const gulp = require('gulp');
 
+
 gulp.task('copy-html', function() {
   return gulp.src('./index.html')
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public'))
+    .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('copy-ico', function() {
+  return gulp.src('./aalogo.ico')
+    .pipe(gulp.dest('./public'))
+    .pipe(gulp.dest('./docs'));
+});
+
+gulp.task('copy-images', function() {
+  return gulp.src('./aalogo.png')
+    .pipe(gulp.dest('./public'))
+    .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('copy-css', function() {
   return gulp.src('./styles.css')
-    .pipe(gulp.dest('./public'));
+    .pipe(gulp.dest('./public'))
+    .pipe(gulp.dest('./docs'));
 });
 
-gulp.task('build', gulp.parallel('copy-html', 'copy-css'));
+gulp.task('build', gulp.parallel('copy-html', 'copy-css', 'copy-ico', 'copy-images'));
